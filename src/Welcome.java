d8b57386d8e81d8f016bc604b7cf5225fcae360b
nguyenkiet2271 <23649011.kiet@student.iuh.edu.vn>
2024-08-14 09:51:15 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Do Anh Kiet");
+        System.out.println("Welcome to the DHKTPM19ATT");
+    }
+}
